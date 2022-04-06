efe2d4392d596e59bf6378565b82645d1a6b487f
EileenTheCrow <eileencatoe@gmail.com>
2022-04-05 20:25:31 -0400

    Initial commit

---
+++ src/main/java/com/athenseats/server/AthensEatsApplication.java
@@ -0,0 +1,13 @@
+package com.athenseats.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthensEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthensEatsApplication.class, args);
+	}
+
+}
