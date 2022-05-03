bce345fb5a398ce1beeca543fe087e4b705a0e6d
vmg16660 <74929798+vmg16660@users.noreply.github.com>
2022-05-03 19:07:08 -0400

    added about us discription

--- src/main/java/com/athenseats/client/src/components/AboutUs/AboutUs.js
+++ src/main/java/com/athenseats/client/src/components/AboutUs/AboutUs.js
@@ -15,19 +15,13 @@ function AboutUs() {
           About Us
         </Typography>
         <Typography className="about-body" variant="body1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+          Athens Eats is a website in which you can find and review all of
+          your favorite Athens restaurants! Please come and leave a review on
+          any of the restaurants on our site. First, you will have to registar
+          an account in order to rate and leave a review. If you do not see your
+          favorite restaurant, be sure to make a wishlist entry so we can add it
+          to the site. Thanks for visiting our website. This website has been made
+          by Eileen Catoe, Chioma Nwogor, and Vince Grano.
         </Typography>
       </div>
     </div>
